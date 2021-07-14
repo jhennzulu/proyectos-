ec5a23d010a7dab65ac5937738172bf3f853b198
jhennzulu <lzuluaga.tgo@gmail.com>
2021-07-13 21:54:16 -0500

    Se crea clase producto con metodos accesorios  y su respectivo objeto

--- tienda/src/App.java
+++ tienda/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("La tienda de ropa");
+        
+        Producto productoUno = new Producto();
+
+        productoUno.setNombre("falda");
+        productoUno.setPrecio(70000);
+        productoUno.mostrar();
+        
     }
 }
--- tienda/src/Producto.java
+++ tienda/src/Producto.java
@@ -1,10 +1,30 @@
 public class Producto {
 
-    private String descripcion = " ";
+    private String nombre = " ";
     private float precio = 0;
 
-    public void mostrar() {
-        System.out.println(descripcion+ ":"+ precio);
+    public Producto(){
+
     }
 
+    public void mostrar() {
+        System.out.println(nombre+ ":"+ precio);
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setPrecio(float precio){
+        this.precio = precio;
+    }
+
+    public float getPrecio(){
+        return this.precio;
+
+    }    
 }
